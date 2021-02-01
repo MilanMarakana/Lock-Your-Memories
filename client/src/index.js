5e79f2d9ace10bc7e9df1fb2d2c00dba7bf1320f
Milan <milan.marakana7@gmail.com>
2021-02-01 02:11:27 +0100

    Completed getting post from DB and view it to screen

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; //Provider give access to use store anywhere in app
 import {createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
+import './index.module.css';
 
 import App from './App';
 import reducers from './Memories_Store/reducers';
